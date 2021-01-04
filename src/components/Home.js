220dc39ec025b3b9012b50e4ecea2752b2a1bb46
David G <extern.david.garcia-ontivero@vwfs.io>
2021-01-04 20:28:35 +0100

    render a initial homePage component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomePage = () => (
+  <header className="homePage-Header">
+    <h1 className="homePage-HeaderTitle">Fullstack Blockchain based project</h1>
+    <a
+      className="homePage-HeaderAuthor"
+      title="developed by David G"
+      href="https://twitter.com/@d4vecarter"
+    >
+      @d4vecarter
+    </a>
+  </header>
+);
+
+export { HomePage };
