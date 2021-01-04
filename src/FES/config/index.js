d1d3d5cf9350a9f6548cb919f5d87906424ee963
David G <extern.david.garcia-ontivero@vwfs.io>
2021-01-04 20:28:56 +0100

    add initial FES config

---
+++ src/FES/config/index.js
@@ -0,0 +1,19 @@
+const LOCALE = {
+  EUR: "es-ES",
+  USD: "en-US",
+};
+
+const GENESIS_BLOCK = {
+  data: "This is the GENESIS block",
+  difficulty: "0000",
+  hash: "0000000000",
+  lastHash: "0000",
+  timestamp: "Initial date",
+};
+
+const config = {
+  LOCALE,
+  GENESIS_BLOCK,
+};
+
+export { config };
---
+++ src/FES/index.js
@@ -0,0 +1,26 @@
+import { config } from "./config";
+
+class Block {
+  constructor({ data, difficulty, hash, lastHash, timestamp }) {
+    this.data = data;
+    this.difficulty = difficulty;
+    this.hash = hash;
+    this.lastHash = lastHash;
+    this.timestamp = timestamp;
+  }
+
+  static genesis() {
+    this._config = config;
+    const {
+      data,
+      difficulty,
+      hash,
+      lastHash,
+      timestamp,
+    } = this._config.GENESIS_BLOCK;
+
+    return new this({ data, difficulty, hash, lastHash, timestamp });
+  }
+}
+
+export { Block };
