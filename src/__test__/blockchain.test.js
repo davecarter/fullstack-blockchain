748d36e1dc608676d0dcc5ba43c1bf7aca1d8c68
David G <extern.david.garcia-ontivero@vwfs.io>
2021-01-05 17:24:51 +0100

    add isValidChain test

--- src/__test__/blockchain.test.js
+++ src/__test__/blockchain.test.js
@@ -2,7 +2,11 @@ import { Blockchain } from "../FES/Blockchain"
 import { Block } from "../FES/Block"
 
 describe("a Blockchain is succesfully build", () => {
-  const blockchain = new Blockchain()
+  let blockchain = new Blockchain()
+
+  beforeEach(() => {
+    blockchain = new Blockchain()
+  })
 
   it("contains a `chain` array instance", () => {
     expect(blockchain.chain instanceof Array).toBe(true)
@@ -17,5 +21,48 @@ describe("a Blockchain is succesfully build", () => {
     blockchain.addBlock({ data })
 
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data)
+  })
+
+  describe("Is a valid chain", () => {
+    describe("when the chain does not starts with a Genesis block", () => {
+      it("should return false", () => {
+        blockchain.chain[0] = { data: "invalid genesis block" }
+        expect(Blockchain.isValidChain()).toBe(false)
+      })
+    })
+
+    describe("when the chain starts with a Genesis block and has multiple Blocks", () => {
+      describe("and a lastHash reference has changed", () => {
+        it("should return false", () => {
+          blockchain.addBlock({ data: "one" })
+          blockchain.addBlock({ data: "two" })
+          blockchain.addBlock({ data: "three" })
+
+          blockchain.chain[2].lastHash = "broken-lastHash"
+        })
+      })
+
+      describe("and the chain contains a block with an invalid field", () => {
+        it("should return false", () => {
+          blockchain.addBlock({ data: "one" })
+          blockchain.addBlock({ data: "two" })
+          blockchain.addBlock({ data: "three" })
+
+          blockchain.chain[2].data = "broken-data"
+
+          expect(Blockchain.isValidChain()).toBe(false)
+        })
+      })
+
+      describe("and the chain does not contains any invalid Blocks", () => {
+        it("returns true", () => {
+          blockchain.addBlock({ data: "one" })
+          blockchain.addBlock({ data: "two" })
+          blockchain.addBlock({ data: "three" })
+
+          expect(Blockchain.isValidChain()).toBe(true)
+        })
+      })
+    })
   })
 })
