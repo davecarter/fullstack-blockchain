d250c0eb321d783f99cb3f1ddf4d250ee9ba0769
David G <dave74@gmail.com>
2021-12-23 19:42:55 +0100

    add test coverage in Block class

--- src/__test__/block.test.js
+++ src/__test__/block.test.js
@@ -1,10 +1,30 @@
 import { Block } from '../FES'
-import { cryptoHash } from '../FES/CryptoHash'
+import { CryptoHash } from '../FES/CryptoHash'
+
+describe('Block', () => {
+  const data = 'this is the block data'
+  const difficulty = 1
+  const hash = 'hash'
+  const lastHash = 'last-hash'
+  const nonce = 1
+  const timestamp = Date.now()
+  const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty })
+
+  it('has a timestamp, lasthash, hash, data, nonce and difficulty', () => {
+    expect(block.data).toEqual(data)
+    expect(block.difficulty).toEqual(difficulty)
+    expect(block.hash).toEqual(hash)
+    expect(block.lastHash).toEqual(lastHash)
+    expect(block.nonce).toEqual(nonce)
+  })
+})
 
 describe('Creates the Genesis Block', () => {
   const data = 'This is the GENESIS block'
   const hash = '0000000000'
   const lastHash = '0000000000'
+  const nonce = 1
+  const timestamp = Date.now()
 
   const genesisBlock = Block.genesis()
 
@@ -16,6 +36,7 @@ describe('Creates the Genesis Block', () => {
     expect(genesisBlock.data).toEqual(data)
     expect(genesisBlock.hash).toEqual(hash)
     expect(genesisBlock.lastHash).toEqual(lastHash)
+    expect(genesisBlock.nonce).toEqual(nonce)
   })
 })
 
@@ -42,6 +63,12 @@ describe('Mines a Block', () => {
 
   it('creates a SHA-256 `hash` based on the proper inputs', () => {
     const { hash } = lastBlock
-    expect(minedBlock.hash).toEqual(cryptoHash(data, hash, minedBlock.timestamp))
+    expect(minedBlock.hash).toEqual(
+      CryptoHash(data, hash, minedBlock.timestamp, minedBlock.nonce, minedBlock.difficulty),
+    )
+  })
+
+  it('sets a hash that matches the difficulty criteria', () => {
+    expect(minedBlock.hash.substring(0, minedBlock.difficulty)).toEqual('0'.repeat(minedBlock.difficulty))
   })
 })
--- src/__test__/crypto.test.js
+++ src/__test__/crypto.test.js
@@ -1,8 +1,8 @@
-import { cryptoHash } from '../FES/CryptoHash'
+import { CryptoHash } from '../FES/CryptoHash'
 import hashed from './fixtures/crypto.json'
 
 describe('Adds SHA256 encryption', () => {
   it('generates a SHA-256 hashed output', () => {
-    expect(cryptoHash('Hello crypto World!')).toEqual(hashed.hash)
+    expect(CryptoHash('Hello crypto World!')).toEqual(hashed.hash)
   })
 })
